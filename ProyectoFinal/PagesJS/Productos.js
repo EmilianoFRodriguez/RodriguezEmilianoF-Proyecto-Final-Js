751153c6ed702f8edb19ad05a638a26e07cadd49
Emiliano F Rodriguez <emilianofederico.mmo@outlook.com>
2022-09-21 23:59:45 -0300

    corrigiendo js

---
+++ ProyectoFinal/PagesJS/Productos.js
@@ -0,0 +1,12 @@
+const productos = [
+    {id: 1, filtro: "congelado", tipo: "milanesa", marca: "NotCo", precio: 1000,},
+    {id: 2, filtro: "congelado", tipo: "hamburguesa", marca: "NotCo", precio: 1200,},
+    {id: 3, filtro: "congelado", tipo: "nuggets", marca: "NotCo", precio: 1500,},
+    {id: 4, filtro: "fruto seco", tipo: "avellana", marca: "NotCo", precio: 500,},
+    {id: 5, filtro: "fruto seco", tipo: "nuez", marca: "NotCo", precio: 350,},
+    {id: 6, filtro: "fruto seco", tipo: "almendra", marca: "NotCo", precio: 600,},
+    {id: 7, filtro: "cereal", tipo: "anillos", marca: "NotCo", precio: 170,},
+    {id: 8, filtro: "cereal", tipo: "copo de maiz", marca: "NotCo", precio: 150,}
+];
+
+
--- ProyectoFinal/index.js
+++ ProyectoFinal/index.js
@@ -1,18 +1,23 @@
-const tracks = [
-    {titulo:'', artista:'neverdogs', genero:'minimal', sello:'', precio:'350'},
-    {titulo:'', artista:'adam beyer', genero:'techno', sello:'', precio:'350'},
-    {titulo:'', artista:'marco carola', genero:'tech house', sello:'', precio:'350'},
-    {titulo:'', artista:'richie hawtin', genero:'techno', sello:'', precio:'350'},
-    {titulo:'', artista:'dennis cruz', genero:'minimal', sello:'', precio:'350'},
-    {titulo:'', artista:'paco osuna', genero:'tech house', sello:'', precio:'350'}
+const productos = [
+    {id: 1, filtro: "congelado", tipo: "milanesa", marca: "NotCo", precio: 1000,},
+    {id: 2, filtro: "congelado", tipo: "hamburguesa", marca: "NotCo", precio: 1200,},
+    {id: 3, filtro: "congelado", tipo: "nuggets", marca: "NotCo", precio: 1500,},
+    {id: 4, filtro: "fruto seco", tipo: "avellana", marca: "NotCo", precio: 500,},
+    {id: 5, filtro: "fruto seco", tipo: "nuez", marca: "NotCo", precio: 350,},
+    {id: 6, filtro: "fruto seco", tipo: "almendra", marca: "NotCo", precio: 600,},
+    {id: 7, filtro: "cereal", tipo: "anillos", marca: "NotCo", precio: 170,},
+    {id: 8, filtro: "cereal", tipo: "copo de maiz", marca: "NotCo", precio: 150,}
 ];
 
 
-let genero = prompt("Ingrese el genero a buscar");
+let buscar = document.getElementById("buscador");
 
-let trackFiltrado = tracks.filter(track => track.genero === genero);
 
-console.log(trackFiltrado);
+
+
+let buscarProducto = productos.filter(producto => producto.tipo === resultado);
+
+console.log(buscarProducto);
 
 //Carrito
 
