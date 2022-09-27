f60e4a8503d1c8790cf90875541dfbb16988135e
Emiliano F Rodriguez <emilianofederico.mmo@outlook.com>
2022-09-26 21:35:22 -0300

    Preparando reentrega

--- ProyectoFinal/PagesJS/Productos.js
+++ ProyectoFinal/PagesJS/Productos.js
@@ -1,12 +1,29 @@
 const productos = [
-    {id: 1, filtro: "congelado", tipo: "milanesa", marca: "NotCo", precio: 1000,},
-    {id: 2, filtro: "congelado", tipo: "hamburguesa", marca: "NotCo", precio: 1200,},
-    {id: 3, filtro: "congelado", tipo: "nuggets", marca: "NotCo", precio: 1500,},
-    {id: 4, filtro: "fruto seco", tipo: "avellana", marca: "NotCo", precio: 500,},
-    {id: 5, filtro: "fruto seco", tipo: "nuez", marca: "NotCo", precio: 350,},
-    {id: 6, filtro: "fruto seco", tipo: "almendra", marca: "NotCo", precio: 600,},
-    {id: 7, filtro: "cereal", tipo: "anillos", marca: "NotCo", precio: 170,},
-    {id: 8, filtro: "cereal", tipo: "copo de maiz", marca: "NotCo", precio: 150,}
+    {id: 1, filtro: "congelados", tipo: "milanesa", marca: "NotCo", precio: 1000,},
+    {id: 2, filtro: "congelados", tipo: "hamburguesa", marca: "NotCo", precio: 1200,},
+    {id: 3, filtro: "congelados", tipo: "nuggets", marca: "Felices las vacas", precio: 1500,},
+    {id: 4, filtro: "frutos secos", tipo: "avellana", marca: "Cerealmix", precio: 500,},
+    {id: 5, filtro: "frutos secos", tipo: "nuez", marca: "Cerealmix", precio: 350,},
+    {id: 6, filtro: "frutos secos", tipo: "almendra", marca: "Cerealmix", precio: 600,},
+    {id: 7, filtro: "cereales", tipo: "anillos", marca: "Frutloops", precio: 170,},
+    {id: 8, filtro: "cereales", tipo: "copo de maiz", marca: "Zucaritas", precio: 150,}
 ];
 
+let lista = document.getElementById("contenedorProductos");
+// let btnFrutos = document.getElementById("btnFrutos")
+// btnFrutos.addEventListener("click") 
 
+for(const producto of productos){
+    let li = document.createElement("li");
+    li.innerHTML =  `<div>
+                        <div>
+                        </div>
+                        <div>
+                            <h3>${producto.tipo}</h3>
+                            <p>Marca: ${producto.marca}</p>
+                            <p>Precio: $${producto.precio}</p>
+                        </div>
+                    </div`
+    ;
+    lista.append(li);
+}
--- ProyectoFinal/PagesJS/carrito.js
+++ ProyectoFinal/PagesJS/carrito.js
@@ -1,1 +1,36 @@
-localStorage.setItem()
+
+
+// const guardarCarrito = (clave, valor) => {localStorage.setItem(clave, valor)};
+
+
+let carritoStorage = localStorage.getItem("carrito");
+let carritoContenedor = document.getElementById("carritoC")
+let carrito = [];
+
+if(carritoStorage){
+    carrito = JSON.parse(carritoStorage) 
+};
+
+if(carrito.length > 0){
+    carrito.forEach(elemento => {
+        let itemCarrito = document.createElement("div");
+        itemCarrito.innerHTML = `<div>
+                                    <div>
+                                    </div>
+                                    <div>
+                                        <h3>${elemento.tipo}</h3>
+                                        <p>Precio: $${elemento.precio}</p>
+                                    </div>
+                                </div`
+    carritoContenedor.append(div);
+    });
+}else {
+    let itemCarrito = document.createElement("div");
+    itemCarrito.innerHTML = `<p> No hay productos en el carrito </p>`
+    carritoContenedor.append(div);
+};
+
+let botonLC = document.getElementById("limpiar");
+botonLC.addEventListener("click", () => {
+    localStorage.clear();
+});
